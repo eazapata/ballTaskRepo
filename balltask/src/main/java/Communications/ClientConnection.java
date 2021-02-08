38e0bba45efc1013ab9fd5a621b3013830e82bb7
Eduardo Antonio Zapata Valero <eazapatavalero@gmail.com>
2021-02-08 13:53:26 +0100

    Prueba paredes

---
+++ balltask/src/main/java/Communications/ClientConnection.java
@@ -0,0 +1,72 @@
+package Communications;
+
+import Communications.Channel;
+import Default.BallTask;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+public class ClientConnection implements Runnable {
+
+
+    private String ip;
+    private int port = 9999;
+    private Channel channel;
+    private boolean running = true;
+    private BallTask ballTask;
+    private int minSleep = 200;
+    private int maxSleep = 400;
+
+    /**
+     * Constructor que recibe el channel por el cual se comunicará con el otro equipo la ip a la que tiene que
+     * conectarse y el balltask para indicar quien inicia la conexión
+     * @param channel objeto a través del cual enviará la información
+     * @param ip ip del equipo remoto a la que se conectará
+     * @param ballTask objeto al que setteara un string para indicar quien incia la conexión.
+     */
+
+    public ClientConnection(Channel channel,String ip, BallTask ballTask) {
+        this.channel = channel;
+        this.ip = ip;
+        this.ballTask = ballTask;
+        Thread clientThread = new Thread(this);
+        clientThread.start();
+    }
+
+    /**
+     * Método encargado de iniciar la conexión con el server
+     */
+    private void startConnection() {
+        Socket socket = null;
+        try {
+         if (!this.channel.isOk()) {
+                socket = new Socket(ip, port);
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                String greeting = "BALLTASK";
+                out.writeUTF(greeting);
+                DataInputStream in = new DataInputStream(socket.getInputStream());
+                String response = in.readUTF();
+                if (!this.channel.isOk() && response.equals("OK")) {
+                    this.channel.setSocket(socket);
+                    this.ballTask.setWindow("client");
+                    System.out.println("Conexion establecida");
+                }
+          }
+        } catch (Exception e) {
+            System.out.println("Setting socket");
+        }
+    }
+
+    @Override
+    public void run() {
+        while (this.running) {
+            try {
+                startConnection();
+                Thread.sleep(this.ballTask.generateRandomInt(this.minSleep,this.maxSleep));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ balltask/src/main/java/Default/Statistics.java
@@ -0,0 +1,56 @@
+package Default;
+
+public class Statistics {
+
+    private int totalBalls;
+    private int insideBH;
+    private int waitingBalls;
+
+    public void setBall() {
+        this.totalBalls += 1;
+
+    }
+
+    public void setInsideBH() {
+        this.insideBH += 1;
+
+    }
+
+    public void setWaitingBalls(){
+        this.waitingBalls +=1;
+    }
+
+
+    public int getTotalBalls() {
+
+        return this.totalBalls;
+    }
+
+    public int getWaitingBalls() {
+        return waitingBalls;
+    }
+
+    public int getInsideBH() {
+
+        return this.insideBH;
+    }
+
+    public void removeBall(){
+        this.totalBalls -=1;
+    }
+
+    public void removeFromBlackHole(){
+        this.insideBH -= 1;
+    }
+
+    public void removeWaitingBalls(){
+        this.waitingBalls -=1;
+    }
+
+    public void resetStatistics(){
+        this.totalBalls = 0;
+        this.waitingBalls = 0;
+        this.insideBH = 0;
+    }
+
+}
